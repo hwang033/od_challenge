afc44358c4547c0ce6641bed3b66a983e63804cd
hwang033 <hwang033@archipelago.ad.cs.fiu.edu>
2015-10-07 10:14:44 -0400

    Add index to int fields

--- listings/models.py
+++ listings/models.py
@@ -5,10 +5,10 @@ class Listings(models.Model):
     """
     street = models.CharField(max_length=255)
     status = models.CharField(max_length=20)
-    price = models.IntegerField()
-    bedrooms = models.IntegerField()
-    bathrooms = models.IntegerField()
-    sq_ft = models.IntegerField()
+    price = models.IntegerField(db_index=True)
+    bedrooms = models.IntegerField(db_index=True)
+    bathrooms = models.IntegerField(db_index=True)
+    sq_ft = models.IntegerField(db_index=True)
     lat = models.DecimalField(max_digits=65, decimal_places=30)
     lng = models.DecimalField(max_digits=65, decimal_places=30)
 
